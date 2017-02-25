3da2809a6413f05c3c3cba774468a6b4bccfcdbd
EmmaKoza <emma.koza@gmail.com>
2017-02-25 16:21:37 -0500

    my first commit

--- express-api-starter/api/posts/controller.js
+++ express-api-starter/api/posts/controller.js
@@ -4,14 +4,14 @@ var posts = [
     title: 'My Awesome Item',
     description: 'This item is amazing! You really want to get this.',
     looking_for: 'Gift cards',
-    posted_by: 'Ryan'
+    posted_by: 'Emma'
   },
   {
     id: 2,
     title: 'Not so great item',
     description: 'Eh, I never use this. Its not so great',
     looking_for: 'Beer, wine',
-    posted_by: 'Jim'
+    posted_by: 'Colin'
   }
 ]
 
